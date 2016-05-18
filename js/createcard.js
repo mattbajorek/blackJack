3b486aae31daec882c956a0c0a0de4a9ef42d7f0
Matthew Bajorek <bajorek.matthew@gmail.com>
2016-05-17 20:14:27 -0400

    Added sizing feature for card

--- js/createcard.js
+++ js/createcard.js
@@ -4,6 +4,7 @@ var symbols = ["\u2660","\u2663","\u2665","\u2666"]; // spades, clubs, hearts, d
 var randomNumber
 var randomSymbol
 var cardNumber = 1;
+var fraction = 1/2;
 
 // Create card steps
 function createcards(amount) {
@@ -14,12 +15,17 @@ function createcards(amount) {
 
         // Make card
         $(".holder").append('<div class="card card' + cardNumber + '"></div>');
+        // Size the card
+        $(".card" + cardNumber).css("height", 350*fraction + "px");
+        $(".card" + cardNumber).css("width", 250*fraction + "px");
 
         // Adds number div classes
         $(".card" + cardNumber).append('<div class="number top-number"></div>');
         $(".card" + cardNumber).append('<div class="number bottom-number"></div>');
         // Changes the number to the button selected
         $(".card" + cardNumber + " .number").text( numbers[randomNumber] );
+        // Size the number
+        $(".card" + cardNumber + " .number").css("font-size", 35*fraction + "px");
         // Positions the number properly on card
         numberPosition( numbers[randomNumber] );
 
@@ -53,16 +59,6 @@ function deletecards() {
     }
 }
 
-// Recreates new card
-/*var recreate = function () {
-    if (!$(".holder").find(".card").length > 0) {
-        $(".holder").append("<div class=\"card\"></div>");
-    } else {
-        $(".card").remove();
-        recreate();
-    }
-}*/
-
 // Positions the number properly on card
 var numberPosition = function(num) {
     var classes = [".card" + cardNumber + " .top-number", ".card" + cardNumber + " .bottom-number"];
@@ -70,13 +66,13 @@ var numberPosition = function(num) {
 
     for (var i=0; i<=1; i++) {
         if ( num > 1 && num < 10 ) {
-            $(classes[i]).css(position[i], "10px");
+            $(classes[i]).css(position[i], "4%"); /*10px horizontal*/
         } else if ( num == "J") {
-            $(classes[i]).css(position[i], "13px");
+            $(classes[i]).css(position[i], "5.2%"); /*13px horizontal*/
         } else if ( num == 10) {
-            $(classes[i]).css(position[i], "2px");
+            $(classes[i]).css(position[i], "0.8%"); /*2px horizontal*/
         } else {
-            $(classes[i]).css(position[i], "5px");
+            $(classes[i]).css(position[i], "2%"); /*5px horizontal*/
         }
     }
 }
@@ -85,8 +81,8 @@ var numberPosition = function(num) {
 var symbolSize = function(sym,size) {
     var pixels = [], symbol = [];
     switch (size) {
-        case "small": pixels = ["45px","40px","50px"]; symbol = [".card" + cardNumber + " .top-symbol",".card" + cardNumber + " .bottom-symbol"]; break;
-        case "large": pixels = ["65px","60px","70px"]; symbol = [".card" + cardNumber + " .center-symbol"]; break;
+        case "small": pixels = [45*fraction + "px", 40*fraction + "px", 50*fraction + "px"]; symbol = [".card" + cardNumber + " .top-symbol",".card" + cardNumber + " .bottom-symbol"]; break;
+        case "large": pixels = [65*fraction + "px", 60*fraction + "px", 70*fraction + "px"]; symbol = [".card" + cardNumber + " .center-symbol"]; break;
     }
     for (var i = 0; i<symbol.length; i++) {
         if ( sym == "\u2665" ) { // unicode for hearts
@@ -99,6 +95,14 @@ var symbolSize = function(sym,size) {
     }
 }
 
+// Loop for positioning center symbol
+var positioning = function(num,top,left) {
+    for (var i=0; i<num.length; i++) {
+        $(".card" + cardNumber + " .center-symbol:eq(" + num[i] +")").css("top", top[i]);
+        $(".card" + cardNumber + " .center-symbol:eq(" + num[i] +")").css("left", left[i]);
+    }
+}
+
 // Adds the center element
 var centerSymbol = function() {
     if (randomNumber < 10) {
@@ -107,8 +111,10 @@ var centerSymbol = function() {
         // Position the symbol
         switch ( numbers[randomNumber] ) {
             case "A": {
-                $(".card" + cardNumber + " .center-symbol").css("top", "140px");
-                $(".card" + cardNumber + " .center-symbol").css("left", "105px");
+                var num = [0];
+                var top = ["40%"]; /*140px vertical*/
+                var left = ["42%"]; /*105px horizontal*/
+                positioning(num,top,left);
             }
             break;
             case "2": {
@@ -117,8 +123,10 @@ var centerSymbol = function() {
             break;
             case "3": {
                 pos2();
-                $(".card" + cardNumber + " .center-symbol:eq(2)").css("top", "140px");
-                $(".card" + cardNumber + " .center-symbol:eq(2)").css("left", "105px");
+                var num = [2];
+                var top = ["40%"]; /*140px vertical*/
+                var left = ["42%"]; /*105px horizontal*/
+                positioning(num,top,left);
             }
             break;
             case "4": {
@@ -139,8 +147,10 @@ var centerSymbol = function() {
             break;
             case "8": {
                 pos7();
-                $(".card" + cardNumber + " .center-symbol:eq(7)").css("top", "190px");
-                $(".card" + cardNumber + " .center-symbol:eq(7)").css("left", "110px");
+                var num = [7];
+                var top = ["54.2857142857%"];   /*190px vertical*/
+                var left = ["44%"];  /*110px horizontal*/
+                positioning(num,top,left);
             }
             break;
             case "9": {
@@ -149,10 +159,10 @@ var centerSymbol = function() {
             break;
             case "10": {
                 pos9();
-                $(".card" + cardNumber + " .center-symbol:eq(4)").css("top", "70px");
-                $(".card" + cardNumber + " .center-symbol:eq(4)").css("left", "105px");
-                $(".card" + cardNumber + " .center-symbol:eq(9)").css("top", "210px");
-                $(".card" + cardNumber + " .center-symbol:eq(9)").css("left", "105px");
+                var num = [4,9];
+                var top = ["20%","60%"]; /*70px, 210px vertical*/
+                var left = ["42%","42%"]; /*105px, 105px horizontal*/
+                positioning(num,top,left);
             }
             break;
         }
@@ -192,48 +202,44 @@ var makeCenterSymbol = function(times) {
 
 // Position functions
 var pos2 = function() {
-    $(".card" + cardNumber + " .center-symbol:eq(0)").css("top", "40px");
-    $(".card" + cardNumber + " .center-symbol:eq(0)").css("left", "105px");
-    $(".card" + cardNumber + " .center-symbol:eq(1)").css("top", "240px");
-    $(".card" + cardNumber + " .center-symbol:eq(1)").css("left", "105px");
+    var num = [0,1];
+    var top = ["11.4285714286%","68.5714285714%"]; /*40px, 240px vertical*/
+    var left = ["42%","42%"]; /*105px, 105px horizontal*/
+    positioning(num,top,left);
 }
 var pos4 = function() {
-    $(".card" + cardNumber + " .center-symbol:eq(0)").css("top", "40px");
-    $(".card" + cardNumber + " .center-symbol:eq(0)").css("left", "55px");
-    $(".card" + cardNumber + " .center-symbol:eq(1)").css("top", "40px");
-    $(".card" + cardNumber + " .center-symbol:eq(1)").css("left", "155px");
-    $(".card" + cardNumber + " .center-symbol:eq(2)").css("top", "240px");
-    $(".card" + cardNumber + " .center-symbol:eq(2)").css("left", "55px");
-    $(".card" + cardNumber + " .center-symbol:eq(3)").css("top", "240px");
-    $(".card" + cardNumber + " .center-symbol:eq(3)").css("left", "155px");
+    var num = [0,1,2,3];
+    var top = ["11.4285714286%","11.4285714286%","68.5714285714%","68.5714285714%"]; /*40px, 40px, 240px, 240px vertical*/
+    var left = ["22%","62%","22%","62%"]; /*55px, 155px, 55px, 155px horizontal*/
+    positioning(num,top,left);
 }
 var pos5 = function() {
     pos4();
-    $(".card" + cardNumber + " .center-symbol:eq(4)").css("top", "140px");
-    $(".card" + cardNumber + " .center-symbol:eq(4)").css("left", "105px");
+    var num = [4];
+    var top = ["40%"]; /*140px vertical*/
+    var left = ["42%"]; /*105px horizontal*/
+    positioning(num,top,left);
 }
 var pos6 = function() {
     pos4();
-    $(".card" + cardNumber + " .center-symbol:eq(4)").css("top", "140px");
-    $(".card" + cardNumber + " .center-symbol:eq(4)").css("left", "55px");
-    $(".card" + cardNumber + " .center-symbol:eq(5)").css("top", "140px");
-    $(".card" + cardNumber + " .center-symbol:eq(5)").css("left", "155px");
+    var num = [4,5];
+    var top = ["40%","40%"]; /*140px, 140px vertical*/
+    var left = ["22%","62%"]; /*55px, 155px horizontal*/
+    positioning(num,top,left);
 }
 var pos7 = function() {
     pos6();
-    $(".card" + cardNumber + " .center-symbol:eq(6)").css("top", "90px");
-    $(".card" + cardNumber + " .center-symbol:eq(6)").css("left", "105px");
+    var num = [6];
+    var top = ["40%"]; /*140px vertical*/
+    var left = ["42%"]; /*105px horizontal*/
+    positioning(num,top,left);
 }
 var pos9 = function() {
     pos5();
-    $(".card" + cardNumber + " .center-symbol:eq(5)").css("top", "100px");
-    $(".card" + cardNumber + " .center-symbol:eq(5)").css("left", "55px");
-    $(".card" + cardNumber + " .center-symbol:eq(6)").css("top", "100px");
-    $(".card" + cardNumber + " .center-symbol:eq(6)").css("left", "155px");
-    $(".card" + cardNumber + " .center-symbol:eq(7)").css("top", "180px");
-    $(".card" + cardNumber + " .center-symbol:eq(7)").css("left", "55px");
-    $(".card" + cardNumber + " .center-symbol:eq(8)").css("top", "180px");
-    $(".card" + cardNumber + " .center-symbol:eq(8)").css("left", "155px");
+    var num = [5,6,7,8];
+    var top = ["28.5714285714%","28.5714285714%","51.4285714286%","51.4285714286%"]; /*100px, 100px, 180px, 180px vertical*/
+    var left = ["22%","62%","22%","62%"]; /*55px, 155px, 55px, 155px horizontal*/
+    positioning(num,top,left);
 }
 
 var changeSuitImage = function() {
@@ -249,19 +255,25 @@ var changeSuitImage = function() {
     }
     // Create picture div
     $(".card" + cardNumber).append('<div class="symbol center-symbol"><img src="images/' + suit + numbers[randomNumber] + '.png"/></div>');
+    // Size the picture
+    $(".card" + cardNumber + " img").css("height", 236*fraction + "px");
+    $(".card" + cardNumber + " img").css("width", 146*fraction + "px");
     // Position the picture
     $(".card" + cardNumber + " .center-symbol:eq(0)").css("position", "absolute");
-    $(".card" + cardNumber + " .center-symbol:eq(0)").css("top", "57px");
-    $(".card" + cardNumber + " .center-symbol:eq(0)").css("left", "50px");
+    var num = [0];
+    var top = ["16.2857142857%"]; /*57px vertical*/
+    var left = ["20%"]; /*50px horizontal*/
+    positioning(num,top,left);
     // Create symbols
     makeCenterSymbol(2);
     // Position the symbols
-    $(".card" + cardNumber + " .center-symbol:eq(1)").css("top", "50px");
-    $(".card" + cardNumber + " .center-symbol:eq(1)").css("left", "55px");
-    $(".card" + cardNumber + " .center-symbol:eq(2)").css("top", "230px");
-    $(".card" + cardNumber + " .center-symbol:eq(2)").css("left", "155px");
+    var num = [1,2];
+    var top = ["14.2857142857%","65.7142857143%"]; /*50px, 230px vertical*/
+    var left = ["22%","62%"]; /*55px, 155px horizontal*/
+    positioning(num,top,left);
+    // Position club properly
     if (suit == "club") {
-        $(".card" + cardNumber + " .center-symbol:eq(2)").css("top", "240px");
+        $(".card" + cardNumber + " .center-symbol:eq(2)").css("top", "68.5714285714%"); /*240px vertical*/
     }
     // Rotate second symbol
     $(".card" + cardNumber + " .center-symbol:eq(2)").css("-ms-transform", "rotate(180deg)");
